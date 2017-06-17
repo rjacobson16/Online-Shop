afef1f9fb78661d24f1552598a0916f380b76111
rachel <rmjacobson1@gmail.com>
2017-06-16 21:46:30 -0700

    initial commit with shop, cart, orders, and login

---
+++ myshop/orders/migrations/0001_initial.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('shop', '0002_auto_20170616_0713'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Order',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
+                ('first_name', models.CharField(max_length=50)),
+                ('last_name', models.CharField(max_length=50)),
+                ('email', models.EmailField(max_length=254)),
+                ('address', models.CharField(max_length=250)),
+                ('postal_code', models.CharField(max_length=20)),
+                ('city', models.CharField(max_length=100)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('updated', models.DateTimeField(auto_now=True)),
+                ('paid', models.BooleanField(default=False)),
+            ],
+            options={
+                'ordering': ('-created',),
+            },
+        ),
+        migrations.CreateModel(
+            name='OrderItem',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
+                ('price', models.DecimalField(max_digits=10, decimal_places=2)),
+                ('quantity', models.PositiveIntegerField(default=1)),
+                ('order', models.ForeignKey(to='orders.Order', related_name='items')),
+                ('product', models.ForeignKey(to='shop.Product', related_name='order_items')),
+            ],
+        ),
+    ]
